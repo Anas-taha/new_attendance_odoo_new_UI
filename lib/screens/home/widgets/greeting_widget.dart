d2c499c54db83bfe63f402d0942c15f58233ebfd
MahmoudReda0o0 <mahmoudreda1321@gmail.com>
2026-04-02 01:04:34 +0200

    app color & custom text

--- lib/screens/home/widgets/greeting_widget.dart
+++ lib/screens/home/widgets/greeting_widget.dart
@@ -4,46 +4,47 @@ import 'package:hr_app_odoo/generated/l10n/app_localizations.dart';
 import 'package:hr_app_odoo/screens/home/home_data.dart';
 
 class GreetingWidget extends StatelessWidget {
-    GreetingWidget({super.key});
+  GreetingWidget({super.key});
   final homeController = Get.find<HomeController>();
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            color: const Color(0xFF6B46C1),
-            borderRadius: BorderRadius.circular(30),
-          ),
-          child: const Icon(Icons.person, size: 30, color: Colors.white),
-        ),
-        const SizedBox(width: 16),
+        // Container(
+        //   width: 60,
+        //   height: 60,
+        //   decoration: BoxDecoration(
+        //     color: const Color(0xFF6B46C1),
+        //     borderRadius: BorderRadius.circular(30),
+        //   ),
+        //   child: const Icon(Icons.person, size: 30, color: Colors.white),
+        // ),
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                '${homeController.getGreeting(context)}, ${homeController.currentEmployee.value?.name ?? AppLocalizations.of(context)!.employee}',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey[800],
-                ),
-              ),
+              
+              // Text(
+              //   '${homeController.getGreeting(context)}, ${homeController.currentEmployee.value?.name ?? AppLocalizations.of(context)!.employee}',
+              //   style: TextStyle(
+              //     fontSize: 20,
+              //     fontWeight: FontWeight.bold,
+              //     color: Colors.grey[800],
+              //   ),
+              // ),
+
+              // Text(
+              //   AppLocalizations.of(context)!.welcomeBackDashboard,
+              //   style: TextStyle(fontSize: 14, color: Colors.grey[600]),
+              // ),
               const SizedBox(height: 4),
-              Text(
-                AppLocalizations.of(context)!.welcomeBackDashboard,
-                style: TextStyle(fontSize: 14, color: Colors.grey[600]),
-              ),
-              const SizedBox(height: 4),
-              Text(
-                AppLocalizations.of(
-                  context,
-                )!.currentTime(homeController.getCurrentTime()),
-                style: TextStyle(fontSize: 12, color: Colors.grey[500]),
-              ),
+              // Text(
+              //   AppLocalizations.of(
+              //     context,
+              //   )!.currentTime(homeController.getCurrentTime()),
+              //   style: TextStyle(fontSize: 12, color: Colors.grey[500]),
+              // ),
             ],
           ),
         ),
---
+++ lib/widgets/CustomText/customText.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hr_app_odoo/theme/app_theme.dart';
+
+class Customtext extends StatelessWidget {
+  Customtext({
+    super.key,
+    required this.text,
+    this.color,
+    this.fontSize,
+    this.fontWeight,
+    this.isBold = false,
+  });
+  String text;
+  Color? color;
+  double? fontSize;
+  FontWeight? fontWeight;
+  bool isBold;
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color ?? AppColors.app1A1A1AText1,
+        fontSize: isBold ? 20 : fontSize ?? 16,
+        fontWeight:isBold ? FontWeight.bold : fontWeight ?? FontWeight.w500,
+      ),
+    );
+  }
+}
